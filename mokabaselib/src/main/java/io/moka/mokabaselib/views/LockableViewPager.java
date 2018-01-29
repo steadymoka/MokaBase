0bd3db3f4f0d7a1f94065afa8e23faeb8c322957
moka-a <aud1220a@gmail.com>
2018-01-29 19:21:09 +0900

    add LockableViewPager

---
+++ mokabaselib/src/main/java/io/moka/mokabaselib/views/LockableViewPager.java
@@ -0,0 +1,48 @@
+package io.moka.mokabaselib.views;
+
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+
+public class LockableViewPager extends ViewPager {
+
+    private boolean lockMode = true;
+
+    public LockableViewPager(Context context) {
+        this(context, null);
+    }
+
+    public LockableViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (!lockMode)
+            return super.onTouchEvent(event);
+
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        if (!lockMode)
+            return super.onInterceptTouchEvent(event);
+
+        return false;
+    }
+
+    public void setLocked(boolean isLocked) {
+        this.lockMode = isLocked;
+    }
+
+    public boolean isLocked() {
+        return lockMode;
+    }
+
+}
