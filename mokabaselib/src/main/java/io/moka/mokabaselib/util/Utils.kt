8c72ea3b06f2abcf56bac8af2350ca3d8f4c63b9
moka-a <aud1220a@gmail.com>
2018-03-16 17:22:50 +0900

    spannable string api is changed

--- mokabaselib/src/main/java/io/moka/mokabaselib/util/Utils.kt
+++ mokabaselib/src/main/java/io/moka/mokabaselib/util/Utils.kt
@@ -1,12 +1,14 @@
 package io.moka.mokabaselib.util
 
 import android.graphics.Color
+import android.graphics.Typeface
 import android.support.v4.content.ContextCompat
 import android.text.SpannableString
 import android.text.SpannableStringBuilder
 import android.text.Spanned
 import android.text.style.ForegroundColorSpan
 import android.text.style.RelativeSizeSpan
+import android.text.style.StyleSpan
 import io.moka.mokabaselib.MokaBase
 
 fun color(resId: Int): Int {
@@ -14,15 +16,16 @@ fun color(resId: Int): Int {
 }
 
 /**
- * first : text
+ * first : first - text, second - isBold
  * second : ratio
  * third : first - color, second - alpha
  */
-fun spannableText(vararg triple: Triple<String, Float, Pair<Int, Float>>): CharSequence {
+fun spannableText(vararg triple: Triple<Pair<String, Boolean>, Float, Pair<Int, Float>>): CharSequence {
     val spannableStringBuilder = SpannableStringBuilder()
 
     triple.forEach {
-        val text = it.first
+        val text = it.first.first
+        val isBold = it.first.second
         val ratio = it.second
         val color = it.third.first
         val alpha = it.third.second
@@ -30,12 +33,14 @@ fun spannableText(vararg triple: Triple<String, Float, Pair<Int, Float>>): CharS
         val textSpannable = SpannableString(text)
         textSpannable.setSpan(ForegroundColorSpan(Color.argb((alpha * 255).toInt(), Color.red(color), Color.green(color), Color.blue(color))), 0, textSpannable.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         textSpannable.setSpan(RelativeSizeSpan(ratio), 0, textSpannable.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        if (isBold)
+            textSpannable.setSpan(StyleSpan(Typeface.BOLD), 0, textSpannable.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         spannableStringBuilder.append(textSpannable)
     }
 
     return spannableStringBuilder
 }
 
-fun attr(text: String, ratio: Float = 1f, colorRes: Int, alpha: Float = 1f): Triple<String, Float, Pair<Int, Float>> {
-    return Triple(text, ratio, Pair(color(colorRes), alpha))
+fun attr(text: String, ratio: Float = 1f, colorRes: Int, alpha: Float = 1f, isBold: Boolean = false): Triple<Pair<String, Boolean>, Float, Pair<Int, Float>> {
+    return Triple(Pair(text, isBold), ratio, Pair(color(colorRes), alpha))
 }
